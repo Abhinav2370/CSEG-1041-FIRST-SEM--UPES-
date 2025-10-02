f61fd237c8f0d7588fd73e4a59ae4cf0deb63abc
Abhinav2370 <abhinavmenon217@gmail.com>
2025-10-02 14:12:32 +0530

    Create Q48.c

---
+++ #100DAYSOFCODES/Q48.c
@@ -0,0 +1,18 @@
+//Q48 Write a program to print the following pattern:
+//1
+//12
+//123
+//1234
+//12345
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for (i = 1; i <= 5; i++) {          // loop for rows
+        for (j = 1; j <= i; j++) {      // print numbers from 1 to i
+            printf("%d", j);
+        }
+        printf("\n");   // move to next line after each row
+    }
+    return 0;
+}
